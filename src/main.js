85083efa49a04f44ee62879887f9c2ddb5a54957
Meital Lazarovich <lazarovich.meital@gmail.com>
2020-09-30 11:44:21 +0300

    first commit basic proj

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
+import './registerServiceWorker'
+import router from './router.js'
+import store from './store.js'
+import './styles/global.scss'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   store,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="home">
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
